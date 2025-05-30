9cacc3e9b1d361eb3caf7f78a520d4246ae5ee7e
UlviyyaMammadova24 <memmedovaulviyya24@gmail.com>
2025-05-30 08:03:01 +0300

    database

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from flask import Flask, request, render_template
 import numpy as np
 import joblib
 import os
+from pymongo import MongoClient
 
 app = Flask(__name__)
 model = joblib.load("iris_model.pkl")
+
+# MongoDB bağlantısı
+mongo_uri = "mongodb+srv://mammadovaulia82:Jungkook_19971997@cluster0.2g47tbp.mongodb.net/tasklist"
+client = MongoClient(mongo_uri)
+db = client["flowerdb"]
+collection = db["predictions"]
 
 @app.route("/")
 def home():
@@ -24,16 +31,25 @@ def predict_form():
             data = np.array([[float(f1), float(f2), float(f3), float(f4)]])
             prediction = model.predict(data)[0]
 
-            # Sonucu ve girilen verileri tekrar form sayfasına gönder
+            # MongoDB'ye kaydet
+            result = {
+                "sepal_length": float(f1),
+                "sepal_width": float(f2),
+                "petal_length": float(f3),
+                "petal_width": float(f4),
+                "predicted_class": int(prediction)
+            }
+            collection.insert_one(result)
+
+            # Sonuçları tekrar form sayfasına gönder
             return render_template("form.html", prediction=prediction, f1=f1, f2=f2, f3=f3, f4=f4)
-        
+
         except Exception as e:
             return render_template("form.html", error=str(e), f1=f1, f2=f2, f3=f3, f4=f4)
 
-    # GET isteği için boş form göster
     return render_template("form.html")
 
-# Render platformu için PORT ayarı
+# Render uyumlu port ayarı
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
