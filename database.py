32f54355ed283addea3ec8d13003395fe0ae36a0
UlviyyaMammadova24 <memmedovaulviyya24@gmail.com>
2025-05-30 08:17:37 +0300

    database new

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 
-# MongoDB bağlantı URI'n (şifre özel karakter içermediği için doğrudan kullanılabilir)
+# Doğru URI
 MONGO_URI = "mongodb+srv://mammadovaulia82:Jungkook_19971997@cluster0.2g47tbp.mongodb.net/tasklist"
 
-# Bağlantıyı oluştur
 client = MongoClient(MONGO_URI)
 
-# Veritabanı ve koleksiyon seçimi
-db = client["flowerdb"]
-collection = db["predictions"]
+# DÜZELTİLDİ: doğru veritabanı ve koleksiyon
+db = client["predictions"]
+collection = db["predicts"]
+
